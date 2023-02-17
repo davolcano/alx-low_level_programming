279db83e1bb1ea741bbb755b74b8a0ff01d06721
Davolcano <alfy4eva@gmail.com>
2023-02-16 19:55:37 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -E $CFILE -o c
+nclude <stdio.h>
+/**
+ *  * main - Entry
+ *   * Return: Always 0
+ *    */
+int main(void)
+{
+	        printf("with proper grammar, but the outcome is a piece of art,/n");
+		        return (0);
+}
